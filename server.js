ad39b1179e3fa799026d8c75a335b19d55cac27f
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2020-11-29 02:27:33 -0300

    script package.json

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import app from './app';
 
-const port = 4000;
-app.listen(process.env.APP_PORT);
+// const port = 4000;
+app.listen(process.env.APP_PORT || 4000, () => {
+  console.log('Api iniciada');
+});
